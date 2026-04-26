32ad7e2dd2300e60c6a76d263df40fe4f68c5d63
Adam Hall <bluestem16173@gmail.com>
2026-04-25 23:55:40 -0400

    Set revalidate=0 on sitemap to bypass Vercel Edge Cache

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,7 @@ import { MetadataRoute } from 'next'
 import { query } from '../lib/db'
 
 export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://homeservicediagnostics.com';
